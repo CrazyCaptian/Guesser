41a5ec2befcfbef10c26131ab8bb7875d8ef2d35
CrazyCaptian <dottage@gmail.com>
2022-04-23 18:50:26 -0500

    updates

--- guesser.sol
+++ guesser.sol
@@ -33,12 +33,14 @@ contract ForgeGuess is VRFConsumerBase {
     mapping(uint256 => address) public betee;
     mapping(uint256 => uint256) public winnings;
     mapping(address => int) public profitz;
-    int public HouseProfitz = 0;
+
     uint256 public randomResult;
     uint256 public unreleased=0;
     uint256 public totalSupply = 1;
     uint256 public wagered = 0;
+
     bool initeds = false;
+
     mapping(address => uint256) private _balances;
     IERC20 public stakedToken = IERC20(0xbF4493415fD1E79DcDa8cD0cAd7E5Ed65DCe7074);
     
@@ -50,16 +52,15 @@ contract ForgeGuess is VRFConsumerBase {
         return _balances[account];
     }
     
-
     string constant _transferErrorMessage = "staked token transfer failed";
     
     /**
      * Constructor inherits VRFConsumerBase
      * 
-     * Network: Kovan
-     * Chainlink VRF Coordinator address: 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9
-     * LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
-     * Key Hash: 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
+     * Network: Mumbai
+     * Chainlink VRF Coordinator address: 0x8C7382F9D8f56b33781fE506E897a4F1e2d17255
+     * LINK token address:                0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+     * Key Hash: 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4
      */
     constructor() 
         VRFConsumerBase(
