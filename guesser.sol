c3e0439db71217757daa867a83a31ff0713b2daa
CrazyCaptian <dottage@gmail.com>
2022-05-05 20:34:16 -0500

    Update guesser.sol

--- guesser.sol
+++ guesser.sol
@@ -225,13 +225,21 @@ contract ForgeGuess is VRFConsumerBase {
 
             estOutput = (100 * 99 * betAmount)/(odds * 100);
 
-            }else if (ratioz < 1000{
+            }else if (ratioz < 1000){
                 
             estOutput = (100 * 995 * betAmount)/(odds * 1000);
 
-            } else{
+            }else if (ratioz < 2000){
                 
             estOutput = (100 * 985 * betAmount)/(odds * 1000);
+
+            }else if (ratioz < 4000){
+                
+            estOutput = (100 * 965 * betAmount)/(odds * 1000);
+
+            }else{
+                
+            estOutput = (100 * 95 * betAmount)/(odds * 1000);
 
             }
             
